6459d13b1f52cfc2e582b2f60e8ec63fdbbf998d
Zuvvo <Zuvvo4@gmail.com>
2022-10-10 16:07:33 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    confirmations: 'users/confirmations',
+  }
+
+  get "about", to: "about#index"
+
+end
